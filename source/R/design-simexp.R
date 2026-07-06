f8403a3e64d88258951b5ddfd18761e8da9066e9
Ben Serrien <ben.serrien@inbo.be>
2026-07-06 14:35:44 +0200

    init experiment 5 (gamma)

--- source/R/design-simexp.R
+++ source/R/design-simexp.R
@@ -1,5 +1,8 @@
-# design of the simulation experiment
+# design of the simulation experiments
 
+
+# -------------------------------------------------------------------------
+# gaussian data, gaussian noise -------------------------------------------
 
 # parameters of the scenario's:
 # should match the arguments of the function sim_data()
@@ -119,3 +122,53 @@ simexp_design4 <- expand_grid(
   ratio_sdmeval_sdmetrain = c(0.8, 1, 1.2, 1.5)
 ) %>% scenario_labeller()
 
+
+
+# -------------------------------------------------------------------------
+# gamma data, gamma noise -------------------------------------------------
+
+# parameters of the scenario's:
+# should match the arguments of the function sim_data_gamma()
+#   sample_size
+#   mu_x
+#   cv_x
+#   alpha
+#   beta
+#   cv_y
+#   ratio_cvmex_cvx
+#   ratio_mey_mex
+#   corr_error
+
+scenario_labeller_gamma <- function(df) {
+  df %>% 
+    mutate(
+      sample_size_label     = paste0("Nsample", sample_size), 
+      mu_x_label            = paste0("Mux", mu_x),
+      cv_x_label            = paste0("CVx", cv_x),
+      alpha_label           = paste0("Alpha", alpha),
+      beta_label            = paste0("Beta", beta),
+      cv_y_label            = paste0("CVy", cv_y),
+      ratio_cvmex_cvx_label = paste0("Taux", ratio_cvmex_cvx),
+      ratio_mey_mex_label   = paste0("Tauxy", ratio_mey_mex),
+      corr_error_label      = paste0("Rho", corr_error)
+    )
+}
+
+
+simexp_design5 <- expand_grid(
+  sample_size     = c(100),
+  # mu_x, cv_x are based on shape = 2.06, rate = 0.013 (ECEC data Eurofins)
+  mu_x            = 158.4615, 
+  cv_x            = 0.696733, 
+  alpha           = 0,
+  beta            = 1,
+  cv_y            = 0.05,
+  ratio_cvmex_cvx = c(0.05),
+  ratio_mey_mex   = 1,
+  corr_error      = 0
+) %>% scenario_labeller_gamma()
+
+
+
+
+ 
