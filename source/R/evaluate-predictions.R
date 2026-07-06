b08a42e78a2e1e60ae6181a02c5d7bfd6b7d1205
Ben Serrien <ben.serrien@inbo.be>
2026-07-06 14:37:18 +0200

    additional prediction metricx for gamma-distributed variables
    PNEG = proportion of predictions that are negative (out of range)

--- source/R/evaluate-predictions.R
+++ source/R/evaluate-predictions.R
@@ -2,7 +2,8 @@
 
 #' @title eval_preds
 #' @param ... set of models to evaluate, passed as different arguments
-eval_preds <- function(...) {
+eval_preds <- function(..., fx_valmetrics) {
+  #browser()
   preds        <- list(...)
   names(preds) <- stringr::str_split_i(
     as.character(as.list(substitute(list(...)))[-1]),
@@ -13,7 +14,7 @@ eval_preds <- function(...) {
       mutate(model = names(preds)[i])
   }
   data.table::rbindlist(
-    lapply(preds, val_metrics)
+    lapply(preds, fx_valmetrics)
   )
 }
 
@@ -32,5 +33,24 @@ val_metrics <- function(df) {
       R2   = 1 - sum((y_obs - yhat)^2) / sum((y_obs - mean(y_obs))^2),
       SDPE = sd(yhat - y_obs),
       PICP = mean(between(y_obs, yhat_ll, yhat_ul))
+    ) 
+}
+
+#' @title val_metrics_gamma
+#' @description
+#' Calculation of the validation metrics
+#' 
+#' @param df data.frame with predictions
+val_metrics_gamma <- function(df) {
+  df %>%
+    summarise(
+      .by = c(model, .rep),
+      MAE  = mean(abs(yhat - y_obs)),
+      RMSE = sqrt(mean((yhat - y_obs)^2)),
+      MPE  = mean(yhat - y_obs),
+      R2   = 1 - sum((y_obs - yhat)^2) / sum((y_obs - mean(y_obs))^2),
+      SDPE = sd(yhat - y_obs),
+      PICP = mean(between(y_obs, yhat_ll, yhat_ul)),
+      PNEG = mean(yhat < 0)
     ) 
 }
