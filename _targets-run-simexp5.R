09946c188e5dc88d519a710cc6c2eae4e39acf6a
Ben Serrien <ben.serrien@inbo.be>
2026-07-09 17:00:26 +0200

    updates

--- _targets-run-simexp5.R
+++ _targets-run-simexp5.R
@@ -27,7 +27,6 @@ tar_prune()
 tar_objects()
 
 
-tar_load(mcmc_linreg_Nsample100_Mux158.4615_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_6a87be3cf2710f36)
 tar_load(mcmcdx_summary)
 
 
@@ -46,4 +45,29 @@ hist(mcmc_data_Nsample100_Mux5_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_
 summary(mcmc_data_Nsample100_Mux5_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_607315fe597c4c84[[1]]$x_true)
 
 curve(dgamma(x, shape = 0.696733^(-2), rate = 0.696733^(-2)/5), from = 0, to = 30)
+
+with(
+  mcmc_data_Nsample100_Mux158.4615_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_83da79d6e79412d9[[1]],
+  plot(x_true, y_true)
+)
+with(
+  mcmc_data_Nsample100_Mux158.4615_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_83da79d6e79412d9[[1]],
+  plot(x_obs, y_obs)
+)
+
+
+tar_load(mcmcdx_summary)
+
+
+
+tar_load(preds_gammareg_Nsample100_Mux5_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_cfe83edd0d1f157c)
+
+
+tar_load(mcmc_gammareg_Nsample100_Mux158.4615_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_84464aa5df84060c)
+
+mcmc_gammareg_Nsample100_Mux158.4615_CVx0.696733_Alpha0_Beta1_CVy0.05_Taux0.05_Tauxy1_Rho0_84464aa5df84060c %>%
+  filter(.rep == "3dd8f772c42420b2") %>%
+  ggplot(aes(shape)) + 
+  geom_histogram()
+
 
