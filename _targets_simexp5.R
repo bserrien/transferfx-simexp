ce298b90a0112213e01a3490b1903766c58198de
Ben Serrien <ben.serrien@inbo.be>
2026-07-09 17:00:08 +0200

    add gamma-GLM

--- _targets_simexp5.R
+++ _targets_simexp5.R
@@ -29,10 +29,12 @@ list(
     names  = all_of(labs),
     
     # for each batch/rep: draw simulated data and fit Bayesian models on it
-    # the function sim_data returns both a training and a validation dataset
+    # the function sim_data_gamma returns a training and a validation dataset
     tar_stan_mcmc_rep_draws(
       name       = mcmc,
-      stan_files = c(here("source/stan/linreg.stan")),
+      stan_files = c(here("source/stan/linreg.stan"),
+                     here("source/stan/linreg_logtrafo.stan"),
+                     here("source/stan/gammareg.stan")),
       data = sim_data_gamma(
         N               = sample_size,
         mu_x            = mu_x,
@@ -53,17 +55,43 @@ list(
     tar_target(preds_linreg,
                predict_linreg(mcmc_linreg, mcmc_data),
                pattern = map(mcmc_linreg, mcmc_data)),
+    tar_target(preds_linreglogtrafo,
+               predict_linreg_logtrafo(mcmc_linreg_logtrafo, mcmc_data),
+               pattern = map(mcmc_linreg_logtrafo, mcmc_data)),
+    tar_target(preds_gammareg,
+               predict_gammareg(mcmc_gammareg, mcmc_data),
+               pattern = map(mcmc_gammareg, mcmc_data)),
     tar_target(
       predeval,
-      eval_preds(preds_linreg, 
+      eval_preds(preds_linreg, preds_linreglogtrafo, preds_gammareg,
                  fx_valmetrics = val_metrics_gamma)
+    ),
+    
+    # MCMC-diagnostics
+    tar_target(mcmcdx_linreg, 
+               mcmc_dx(mcmc_linreg), 
+               pattern = map(mcmc_linreg)),
+    tar_target(mcmcdx_linreglogtrafo, 
+               mcmc_dx(mcmc_linreg_logtrafo), 
+               pattern = map(mcmc_linreg_logtrafo)),
+    tar_target(mcmcdx_gammareg,
+               mcmc_dx(mcmc_gammareg),
+               pattern = map(mcmc_gammareg)),
+    tar_target(
+      mcmcdx,
+      combine_mcmcdx(mcmcdx_linreg, mcmcdx_linreglogtrafo, mcmcdx_gammareg)
     )
   ),
   
   # combine results across scenario's
   tar_combine(
     predeval_summary,
-    mapped[["predeval"]],
+    mapped[["predeval"]], 
+    command = bind_rows(!!!.x, .id = "scenario")
+  ),
+  tar_combine(
+    mcmcdx_summary,
+    mapped[["mcmcdx"]],
     command = bind_rows(!!!.x, .id = "scenario")
   )
 )
