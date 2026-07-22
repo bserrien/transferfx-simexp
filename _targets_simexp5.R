d41debe9592f07aa39a32257e5df8055de14ee4a
Ben Serrien <ben.serrien@inbo.be>
2026-07-22 08:46:09 +0200

    finished experiment 5

--- _targets_simexp5.R
+++ _targets_simexp5.R
@@ -115,6 +115,12 @@ list(
     mcmcdx_summary,
     mapped[["mcmcdx"]],
     command = bind_rows(!!!.x, .id = "scenario") %>% tidy_scenario_gamma()
+  ),
+  
+  # render a quarto report of the experiment
+  tar_quarto(
+    report_simexp5,
+    path = here("source/quarto/analysis-simexp5.qmd")
   )
 )
 
