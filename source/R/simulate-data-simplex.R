1387339f031268443bf791b2be08d935bee63213
Ben Serrien <ben.serrien@inbo.be>
2026-07-29 14:33:25 +0200

    first version sim_data_simplex

---
+++ source/R/simulate-data-simplex.R
@@ -0,0 +1,67 @@
+# simulate-data-simplex
+
+# simulate data for TF of simplex data: clay + silt + sand = 1
+
+sim_data_simplex <- function(
+  N         = 100,
+  mu_ilr    = c(.5, .25),
+  cov_ilr   = matrix(c(.16, -.14, -.14, 1.34), byrow = T, nrow = 2),
+  alpha_ilr = c(.2, .5),
+  beta_ilr  = c(.9, 1.2),
+  sigma_ilr = c(.1, .1),
+  sd_mex1   = .1,
+  sd_mex2   = .1,
+  ratio_sdmey_sdmex = 1,
+  corr_mexy         = 0
+) {
+  
+  # simulate latent values for the non-reference method
+  ilr_x <- data.frame(MASS::mvrnorm(N, mu_ilr, cov_ilr))
+
+  # regression to latent values of the reference method
+  ilr_y <- data.frame(
+    Y1 = rnorm(N, alpha_ilr[1] + beta_ilr[1] * ilr_x$X1, sigma_ilr[1]),
+    Y2 = rnorm(N, alpha_ilr[2] + beta_ilr[2] * ilr_x$X2, sigma_ilr[2])
+  )
+  
+  # transform latent to PSD (%)
+  psd_x <- data.frame(compositions::ilrInv(ilr_x))
+  colnames(psd_x) <- c("x_true_clay","x_true_silt","x_true_sand")
+  psd_y <- data.frame(compositions::ilrInv(ilr_y))
+  colnames(psd_y) <- c("y_true_clay","y_true_silt","y_true_sand")
+  psd <- cbind(psd_x, psd_y)
+  
+  # add measurement noise: additive noise on ILR-scale
+  sd_mey1 <- sd_mex1 * ratio_sdmey_sdmex
+  sd_mey2 <- sd_mex2 * ratio_sdmey_sdmex
+  eps_xy <- MASS::mvrnorm(
+    n     = N, 
+    mu    = rep(0, 4), 
+    Sigma = matrix(
+      c(sd_mex1^2, 0, corr_mexy * sd_mex1 * sd_mey1, 0,
+        0, sd_mex2^2, 0, corr_mexy * sd_mex2 * sd_mey2,
+        corr_mexy * sd_mex1 * sd_mey1, 0, sd_mey1^2, 0,
+        0, corr_mexy * sd_mex2 * sd_mey2, 0, sd_mey2^2), 
+      byrow = T, nrow = 4
+    )
+  )
+  ilr_x_obs <- ilr_x + eps_xy[, 1:2]
+  ilr_y_obs <- ilr_y + eps_xy[, 3:4]
+  
+  # transform observed to PSD (%)
+  psd_x_obs <- data.frame(compositions::ilrInv(ilr_x_obs))
+  colnames(psd_x_obs) <- c("x_obs_clay","x_obs_silt","x_obs_sand")
+  psd_y_obs <- data.frame(compositions::ilrInv(ilr_y_obs))
+  colnames(psd_y_obs) <- c("y_obs_clay","y_obs_silt","y_obs_sand")
+  psd_obs <- cbind(psd_x_obs, psd_y_obs)
+  
+  return(cbind(psd, psd_obs))
+}
+
+
+ggtern(psd) +
+  geom_point(aes(y_silt, y_clay, y_sand), color = "red") +
+  geom_point(aes(x_silt, x_clay, x_sand), color = "blue") 
+
+ggplot(psd, aes(x_sand, y_sand)) +
+  geom_point()
