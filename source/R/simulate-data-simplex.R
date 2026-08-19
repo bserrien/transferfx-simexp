7bf51230beb565ce7b95dbfd9d602cb02d5c9d6c
hansvancalster <hans.vancalster@inbo.be>
2026-08-19 14:01:39 +0200

    Revert "comment out testing code (to avoid tar_source errorring)"
    This reverts commit de90d92d72db0d93debfdbce8095ad44cdea4960.

--- source/R/simulate-data-simplex.R
+++ source/R/simulate-data-simplex.R
@@ -107,123 +107,123 @@ sim_data_simplex <- function(
 }
 
 
-# # testing
-# psd <- sim_data_simplex()
-# 
-# spc <- data.frame(
-#   .name = c("x_obs_clay","x_obs_silt","x_obs_sand",
-#             "y_obs_clay","y_obs_silt","y_obs_sand",
-#             "x_true_clay","x_true_silt","x_true_sand",
-#             "y_true_clay","y_true_silt","y_true_sand"),
-#   .value = rep(c("clay","silt","sand"), 4)
-# ) %>%
-#   mutate(method = substr(.name, 1, 1),
-#          obs = stringr::str_split_i(.name, "_", 2))
-# 
-# b <- psd$psd %>%
-#   pivot_longer_spec(spc) %>%
-#   ggtern::ggtern(aes(silt, clay, sand, group = id)) +
-#   facet_wrap(~ obs) +
-#   geom_point(aes(color = method)) + geom_line(alpha = .2)
-# 
-# a <- psd$psd %>%
-#   pivot_longer_spec(spc) %>%
-#   pivot_longer(c("clay","silt","sand"),
-#                names_to = "ps", values_to = "pct") %>%
-#   pivot_wider(values_from = pct,
-#               names_from = method, names_prefix = "pct_") %>%
-#   ggplot(aes(pct_x, pct_y, color = obs)) +
-#   facet_wrap(~ ps) +
-#   geom_point() +
-#   geom_abline(intercept = 0, slope = 1, lty = 2)
-# 
-# ggpubr::ggarrange(a, b, nrow = 2)
-# 
-# 
-# library(cmdstanr)
-# 
-# mod <- cmdstan_model(here::here("source/stan-PSD/mvlinreg.stan"))
-# 
-# fit <- mod$sample(data = psd)
-# 
-# fitsum <- fit$summary()
-# fitsum
-# 
-# draws_df <- fit$draws(format = "data.frame")
-# 
-# 
-# 
-# predict_psd <- function(draws_ilr) {
-#   
-#   draws_new_obs <- draws_ilr %>%
-#     select(.draw, contains("ilr_y_obs_new_rep"))
-#   
-#   spc <- data.frame(
-#     .name = colnames(draws_new_obs)[2:ncol(draws_new_obs)]
-#   ) %>%
-#     mutate(
-#       .value = case_when(grepl(",1]", .name) ~ "X1",
-#                          grepl(",2]", .name) ~ "X2"),
-#       obs = stringr::str_extract(.name, "(?<=\\[)\\d+")
-#     )
-#   
-#   draws_new_obs_long <- draws_new_obs %>%
-#     pivot_longer_spec(spc)
-#   draws_new_obs_psd <- compositions::ilrInv(
-#     draws_new_obs_long[, c("X1","X2")]
-#   )
-#   colnames(draws_new_obs_psd) <- c("clay","silt","sand")
-#   
-#   pred_summary <- cbind(draws_new_obs_long, draws_new_obs_psd) %>%
-#     summarise(
-#       .by = obs,
-#       clay_hat = mean(clay),
-#       silt_hat = mean(silt),
-#       sand_hat = mean(sand),
-#       clay_ll  = quantile(clay, .025),
-#       clay_ul  = quantile(clay, .975),
-#       silt_ll  = quantile(silt, .025),
-#       silt_ul  = quantile(silt, .975),
-#       sand_ll  = quantile(sand, .025),
-#       sand_ul  = quantile(sand, .975)
-#     )
-# 
-#   return( pred_summary )
-# }
-# 
-# preds_psd <- predict_psd(draws_df)
-# 
-# 
-# 
-# 
-# 
-# 
-# idx <- sample(1:nrow(preds_psd), 2)
-# psd$psd_new[idx, ] %>% select(contains("y_")) %>% print()
-# preds_psd[idx, ] %>% print()
-# preds_psd[idx, ] %>%
-#   ggtern::ggtern(aes(silt_hat, clay_hat, sand_hat)) +
-#   geom_point() +
-#   geom_errorbarL(aes(Lmin = silt_ll, Lmax = silt_ul)) +
-#   geom_errorbarT(aes(Tmin = clay_ll, Tmax = clay_ul)) +
-#   geom_errorbarR(aes(Rmin = sand_ll, Rmax = sand_ul)) +
-#   geom_point(
-#     data = psd$psd_new[idx, ], 
-#     aes(y_obs_silt, y_obs_clay, y_obs_sand), 
-#     color = "red"
-#   )
-# 
-# 
-# 
-# cbind(psd$psd_new, preds_psd) %>%
-#   summarise(
-#     MPE_clay = mean(y_obs_clay - clay_hat),
-#     MPE_silt = mean(y_obs_silt - silt_hat),
-#     MPE_sand = mean(y_obs_sand - sand_hat),
-#     PICP_clay = mean(between(y_obs_clay, clay_ll, clay_ul)),
-#     PICP_silt = mean(between(y_obs_silt, silt_ll, silt_ul)),
-#     PICP_sand = mean(between(y_obs_sand, sand_ll, sand_ul))
-#   )
-# 
-# 
-# 
+# testing
+psd <- sim_data_simplex()
+
+spc <- data.frame(
+  .name = c("x_obs_clay","x_obs_silt","x_obs_sand",
+            "y_obs_clay","y_obs_silt","y_obs_sand",
+            "x_true_clay","x_true_silt","x_true_sand",
+            "y_true_clay","y_true_silt","y_true_sand"),
+  .value = rep(c("clay","silt","sand"), 4)
+) %>%
+  mutate(method = substr(.name, 1, 1),
+         obs = stringr::str_split_i(.name, "_", 2))
+
+b <- psd$psd %>%
+  pivot_longer_spec(spc) %>%
+  ggtern::ggtern(aes(silt, clay, sand, group = id)) +
+  facet_wrap(~ obs) +
+  geom_point(aes(color = method)) + geom_line(alpha = .2)
+
+a <- psd$psd %>%
+  pivot_longer_spec(spc) %>%
+  pivot_longer(c("clay","silt","sand"),
+               names_to = "ps", values_to = "pct") %>%
+  pivot_wider(values_from = pct,
+              names_from = method, names_prefix = "pct_") %>%
+  ggplot(aes(pct_x, pct_y, color = obs)) +
+  facet_wrap(~ ps) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, lty = 2)
+
+ggpubr::ggarrange(a, b, nrow = 2)
+
+
+library(cmdstanr)
+
+mod <- cmdstan_model(here::here("source/stan-PSD/mvlinreg.stan"))
+
+fit <- mod$sample(data = psd)
+
+fitsum <- fit$summary()
+fitsum
+
+draws_df <- fit$draws(format = "data.frame")
+
+
+
+predict_psd <- function(draws_ilr) {
+  
+  draws_new_obs <- draws_ilr %>%
+    select(.draw, contains("ilr_y_obs_new_rep"))
+  
+  spc <- data.frame(
+    .name = colnames(draws_new_obs)[2:ncol(draws_new_obs)]
+  ) %>%
+    mutate(
+      .value = case_when(grepl(",1]", .name) ~ "X1",
+                         grepl(",2]", .name) ~ "X2"),
+      obs = stringr::str_extract(.name, "(?<=\\[)\\d+")
+    )
+  
+  draws_new_obs_long <- draws_new_obs %>%
+    pivot_longer_spec(spc)
+  draws_new_obs_psd <- compositions::ilrInv(
+    draws_new_obs_long[, c("X1","X2")]
+  )
+  colnames(draws_new_obs_psd) <- c("clay","silt","sand")
+  
+  pred_summary <- cbind(draws_new_obs_long, draws_new_obs_psd) %>%
+    summarise(
+      .by = obs,
+      clay_hat = mean(clay),
+      silt_hat = mean(silt),
+      sand_hat = mean(sand),
+      clay_ll  = quantile(clay, .025),
+      clay_ul  = quantile(clay, .975),
+      silt_ll  = quantile(silt, .025),
+      silt_ul  = quantile(silt, .975),
+      sand_ll  = quantile(sand, .025),
+      sand_ul  = quantile(sand, .975)
+    )
+
+  return( pred_summary )
+}
+
+preds_psd <- predict_psd(draws_df)
+
+
+
+
+
+
+idx <- sample(1:nrow(preds_psd), 2)
+psd$psd_new[idx, ] %>% select(contains("y_")) %>% print()
+preds_psd[idx, ] %>% print()
+preds_psd[idx, ] %>%
+  ggtern::ggtern(aes(silt_hat, clay_hat, sand_hat)) +
+  geom_point() +
+  geom_errorbarL(aes(Lmin = silt_ll, Lmax = silt_ul)) +
+  geom_errorbarT(aes(Tmin = clay_ll, Tmax = clay_ul)) +
+  geom_errorbarR(aes(Rmin = sand_ll, Rmax = sand_ul)) +
+  geom_point(
+    data = psd$psd_new[idx, ], 
+    aes(y_obs_silt, y_obs_clay, y_obs_sand), 
+    color = "red"
+  )
+
+
+
+cbind(psd$psd_new, preds_psd) %>%
+  summarise(
+    MPE_clay = mean(y_obs_clay - clay_hat),
+    MPE_silt = mean(y_obs_silt - silt_hat),
+    MPE_sand = mean(y_obs_sand - sand_hat),
+    PICP_clay = mean(between(y_obs_clay, clay_ll, clay_ul)),
+    PICP_silt = mean(between(y_obs_silt, silt_ll, silt_ul)),
+    PICP_sand = mean(between(y_obs_sand, sand_ll, sand_ul))
+  )
+
+
+
